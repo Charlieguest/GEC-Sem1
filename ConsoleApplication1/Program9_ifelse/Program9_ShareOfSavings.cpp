fe10d6da6b36acd6839fa7a34dd98c1338f29a38
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-10-16 14:08:55 +0000

    Added base code

---
+++ ConsoleApplication1/Program9_ifelse/Program9_ShareOfSavings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int mySavings = 2000;
+	int yourPercentage = 50;
+	int yourShare = (mySavings * (yourPercentage / 100));
+
+	cout << "your share: " << yourShare << endl;
+	return 0;
+}
