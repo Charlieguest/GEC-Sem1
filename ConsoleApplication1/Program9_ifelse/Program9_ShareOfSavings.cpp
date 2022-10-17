d6b07d7affe37ebb3f058d27d31cdbe914f8aa10
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-10-17 17:14:32 +0000

    Updated program 9

--- ConsoleApplication1/Program9_ifelse/Program9_ShareOfSavings.cpp
+++ ConsoleApplication1/Program9_ifelse/Program9_ShareOfSavings.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	int mySavings = 2000;
+	// This has to be a float instead of an int as ints always end a whole number when divided.
 	float yourPercentage = 50.0f;
 	int yourShare = (mySavings * (yourPercentage / 100));
 
