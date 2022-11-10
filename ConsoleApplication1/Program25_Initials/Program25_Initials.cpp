8450d57ad23c21a9cacfdc9ee979910ce2d42fb9
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-11-10 16:07:14 +0000

    Tutorial Code

---
+++ ConsoleApplication1/Program25_Initials/Program25_Initials.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+/*
+int main()
+{
+    char testString[22] =
+    {
+        'T','h','i','s',' ',
+        'i','s',' ',
+        't','e','s','t',' ',
+        's','t','r','i','n','g',' ', '1',
+        '\0'
+    };
+
+    char testString2[] = "This is a test string 2";
+
+    cout << "teststring1: " << testString << endl;
+    cout << "teststring2: " << testString2 << endl;
+
+    return 0;
+}
+*/
+/*
+int main() 
+{
+    char stringToCopy[] = "TestString";
+    char stringToChange[20] = "This is a ";
+
+    cout << "Before concatenation" << endl;
+    cout << "stringToCopy contains " << stringToCopy << endl;
+    cout << "stringToChange contains " << stringToChange << endl;
+
+    strcat_s( stringToChange, stringToCopy );
+
+    cout << "After concatenation" << endl;
+    cout << "stringToCopy contains " << stringToCopy << endl;
+    cout << "stringToChange contains " << stringToChange << endl;
+}
+*/
