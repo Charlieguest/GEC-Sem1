3c62cf148174b5bbbe53dbc57b0fcd58660abf3c
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-11-24 16:17:18 +0000

    Program 34 [WIP]

---
+++ ConsoleApplication1/Program34_ReplaceCommasWithSpaces/Program34_ReplaceCommasWithSpaces.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void comma2blank(char* textToChange);
+
+int main()
+{
+	char text;
+
+	cout << "enter some text, in place of spaces, use a comma " << endl;
+	cin >> text;
+
+	comma2blank(&text);
+}
+
+void cooma2Blank()
+{
+
+}
