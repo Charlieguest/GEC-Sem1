2265f9b85ba35b7531ae9da6401d5f1ab15ea135
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-12-07 23:41:06 +0000

    Program38 [WIP]

--- ConsoleApplication1/Program38-AccountHolding/Program38-AccountHolding.cpp
+++ ConsoleApplication1/Program38-AccountHolding/Program38-AccountHolding.cpp
@@ -16,12 +16,13 @@ int main()
     bool accountIs;
     bool AccountIsnt;
     int userChoice = 0;
-    char moreDetails;
+    char moreDetails = 'y';
     ofstream outFile;
     ifstream inFile;
 
     while (userChoice != 4)
     {
+        moreDetails = 'y';
         cout << "Welcome to the Bank Here are your options:" << endl;
         cout << endl;
         cout << "(1) Enter new details" << endl;
@@ -33,7 +34,29 @@ int main()
         switch (userChoice)
         {
             case 1:
-                cout << "cheese" << endl;
+                outFile.open("bank.txt", std::ios_base::app);
+                while (outFile.is_open())
+                {
+                    if (moreDetails == 'y')
+                    {
+                        cout << "Enter Your name for the account" << endl;
+                        cin >> account1.name;
+                        cout << "Enter your account number" << endl;
+                        cin >> account1.accountNumber;
+                        cout << "Enter Your balence" << endl;
+                        cin >> account1.balence;
+                        cout << "Thank you!" << endl;
+                        cout << endl;
+                        cout << "Do you wish to answer any more details" << endl;
+                        cout << "Please answer y/n" << endl;
+                        cin >> moreDetails;
+
+                    }
+                    else
+                    {
+                        outFile.close();
+                    }
+                }
             break;
             default:
                 cout << "Cheese paste" << endl;
