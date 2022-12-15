d74ef750da89457f98d1270af4ba3f7c4198a446
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2022-12-15 16:34:31 +0000

    Program39 1st try solution

--- ConsoleApplication1/Program39_TopTenScores/Program39_TopTenScores.cpp
+++ ConsoleApplication1/Program39_TopTenScores/Program39_TopTenScores.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 void ScoreEntry();
 void DisplayScores();
 
 struct Score {
-    string nameArray[10];
-    int scoreArray[10];
     string name;
     int score;
+    string nameArray[10];
+    int scoreArray[10];
 };
 
 int main()
 {
     int choice = 0;
     int scoreAmount = 1;
-    int smallestValue;
+    int smallestValue = 0;
     Score entry;
-    fstream inOutFile;
+    ifstream inFile;
+    ofstream outPutStream;
+    string line;
 
     while (choice != 3)
     {
@@ -32,12 +35,17 @@ int main()
         switch (choice)
         {
             case 1:
-                inOutFile.open("Scores.txt", ios_base::app);
-                if (inOutFile.is_open())
+                inFile.open("Scores.txt", ios::app);
+                if (inFile.is_open())
                 {
                     cout << endl;
                     cout << "Please enter a Score" << endl;
                     cin >> entry.score;
+                    while (!inFile.eof())
+                    {
+                        getline(inFile, line);
+                        scoreAmount++;
+                    }
                     if (scoreAmount == 1)
                     {
                         smallestValue = entry.score;
@@ -49,37 +57,49 @@ int main()
                             smallestValue = entry.score;
                         }
                     }
-                    cout << "Please enter a Name" << endl;
-                    cin >> entry.name;
-                    if (scoreAmount >= 10)
+
+                    inFile.close();
+                }
+                cout << "Please enter a Name" << endl;
+                cin >> entry.name;
+                if (scoreAmount >= 10)
+                {
+                    for (int i = 0; i < 10; i++)
                     {
-                        for (int i = 0; i < 10; i++)
+                        if (entry.scoreArray[i] == smallestValue)
                         {
-                            if (entry.scoreArray[i] == smallestValue)
-                            {
-                                // replace smallest value in my current array
-                            }
+                            // replace smallest value in my current array
                         }
-                        
                     }
-                    else
+                        
+                }
+                else
+                {
+                    outPutStream.open("Scores.txt", ios_base::app);
+                    if (outPutStream.is_open())
                     {
                         for (int i = scoreAmount - 1; i < scoreAmount; i++)
                         {
+                            cout << scoreAmount << endl;
                             entry.scoreArray[i] = entry.score;
                             entry.nameArray[i] = entry.name;
-                            cout << entry.scoreArray[i] << endl;
-                            inOutFile << "Name: " << entry.nameArray[i] << " || ";
-                            inOutFile << "Score: " << entry.scoreArray[i] << endl;
-
+                            outPutStream << "Name: " << entry.nameArray[i] << " || ";
+                            outPutStream << "Score: " << entry.scoreArray[i] << endl;
                         }
                     }
-                   
-
-                    scoreAmount++;
-
                 }
-                inOutFile.close();
+            break;
+            case 2:
+                inFile.open("Scores.txt");
+                if (inFile.is_open())
+                {
+                    while (!inFile.eof())
+                    {
+                        getline(inFile, line);
+                        cout << line << endl;
+                    }
+                }
+                inFile.close();
             break;
             default:
                 cout << "cheese";
